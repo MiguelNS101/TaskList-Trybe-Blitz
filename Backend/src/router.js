793fb2f2045dce2ed1c64f9f53d4ce5ea426d41b
MiguelNS <MiguelNS101@gmail.com>
2022-06-28 18:00:33 -0300

    configure router

--- Backend/src/router.js
+++ Backend/src/router.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const TaskController = require('./controllers/task.controller');
 const StatusController = require('./controllers/task.controller');
-const validationTask = require('./middlewares/task.middlewares');
+const validationTask = require('./middlewares/task.middleware');
 
 const router = express.Router();
 
 router.get('/getTasks', TaskController.getAll);
 router.get('/getStatus', StatusController.getAll);
-router.post('/createTask', validationTask, TaskController.create);
-router.put('/updateTask/:id', validationTask, TaskController.update);
-router.put('/updateStatus/:id', TaskController.updateStatus);
-router.delete('/deleteTask/:id', TaskController.remove);
+router.post(
+  '/createTask',
+  validationTask.validateName,
+  validationTask.validateMessage,
+  validationTask.validateNameLenght,
+  validationTask.validateMessageLenght,
+  TaskController.createTask,
+);
+
+router.put(
+  '/updateTask/:id',
+  validationTask.validateName,
+  validationTask.validateMessage,
+  validationTask.validateNameLenght,
+  validationTask.validateMessageLenght,
+  TaskController.editTask,
+);
+
+router.put('/updateStatus/:id', TaskController.editStatus);
+router.delete('/deleteTask/:id', TaskController.deleteTask);
 
 module.exports = router;
