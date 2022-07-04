d28b9bc178b6bc1934b701dfee9e449ae18924df
MiguelNS <MiguelNS101@gmail.com>
2022-07-04 14:47:59 -0300

    CSS desktop + mobile

--- frontend/src/components/Tasklist.jsx
+++ frontend/src/components/Tasklist.jsx
@@ -67,7 +67,7 @@ class Tasklist extends React.Component {
 
   handleSort = (a, b) => {
     const { sortBy } = this.state;
-    if (sortBy === 'task_status_id') {
+    if (sortBy === 'task_status_id' && sortBy === 'task_id') {
       return a[sortBy] - b[sortBy];
     }
 
@@ -92,33 +92,34 @@ class Tasklist extends React.Component {
     const taskList = tasks.data;
     return (
       <main>
-        <h1>Task List</h1>
-        <form data-testid="createTask">
-          <label htmlFor="newTitle">
-            Titulo:
-            <input
-              type="text"
-              value={newTitle}
-              name="newTitle"
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="newMessage">
-            Mensagem:
-            <input
-              type="text"
-              value={newMessage}
-              name="newMessage"
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="button" onClick={this.addTask}>
-            Create
-          </button>
-        </form>
-        <div data-testid="sortTask">
+        <article className="create">
+          <form data-testid="createTask">
+            <label htmlFor="newTitle">
+              Titulo:
+              <input
+                type="text"
+                value={newTitle}
+                name="newTitle"
+                onChange={this.handleChange}
+              />
+            </label>
+            <label htmlFor="newMessage">
+              Mensagem:
+              <input
+                type="text"
+                value={newMessage}
+                name="newMessage"
+                onChange={this.handleChange}
+              />
+            </label>
+            <button type="button" onClick={this.addTask}>
+              Create
+            </button>
+          </form>
+        </article>
+        <div data-testid="sortTask" className="sorting">
           <label htmlFor="chooseSort">
-            Choose Sorting method
+            Choose Sorting method:
             <select
               onChange={({ target }) => this.chooseSorting(target)}
               value={sortBy}
@@ -130,47 +131,51 @@ class Tasklist extends React.Component {
             </select>
           </label>
         </div>
-        <section data-testid="taskList">
-          {taskList !== undefined ? (
-            taskList.sort(this.handleSort).map(
-              ({
-                task_id,
-                task_name,
-                task_message,
-                task_date,
-                task_status_id,
-              }) => (
-                <div key={task_id}>
-                  <div>
-                    <TaskTitle title={task_name} />
-                    <TaskStatus
-                      status={task_status_id}
-                      id={task_id}
-                      statusList={status.data}
-                    />
-                  </div>
-                  <TaskMessage message={task_message} />
-                  <div>
-                    <h5>{task_date}</h5>
-                    <button
-                      type="button"
-                      onClick={async () => this.delTask(task_id)}
-                    >
-                      Delete
-                    </button>
-                    <button
-                      type="button"
-                      onClick={async () => this.edit(task_id)}
-                    >
-                      Edit
-                    </button>
-                  </div>
-                </div>
-              ),
-            )
-          ) : (
-            <p>Nenhum Task foi encontrado</p>
-          )}
+        <section>
+          <section data-testid="taskList" className="taskList">
+            {taskList !== undefined ? (
+              taskList.sort(this.handleSort).map(
+                ({
+                  task_id,
+                  task_name,
+                  task_message,
+                  task_date,
+                  task_status_id,
+                }) => (
+                  <article key={task_id} className="taskCard">
+                    <div className="titleStatus">
+                      <TaskTitle title={task_name} />
+                      <TaskStatus
+                        status={task_status_id}
+                        id={task_id}
+                        statusList={status.data}
+                      />
+                    </div>
+                    <div className="messageDate">
+                      <TaskMessage message={task_message} />
+                      <h5>{task_date}</h5>
+                    </div>
+                    <div className="taskButtons">
+                      <button
+                        type="button"
+                        onClick={async () => this.delTask(task_id)}
+                      >
+                        Delete
+                      </button>
+                      <button
+                        type="button"
+                        onClick={async () => this.edit(task_id)}
+                      >
+                        Edit
+                      </button>
+                    </div>
+                  </article>
+                ),
+              )
+            ) : (
+              <p>Nenhum Task foi encontrado</p>
+            )}
+          </section>
         </section>
       </main>
     );
